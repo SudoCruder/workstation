2bada0ac975bf80e6b7e5a26f87428e60e7dae21
pycloudts <pycloudts@gmail.home>
2022-11-11 20:25:48 +0100

    more questions_answers in quiz section

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,3 @@
-let user = "PyCloudTs";
+let user = {name: "SudoCoder", email: "SudoCoder@email.com"};
 
-showMessage("message",`Lets get to work! ${user}`)
+showMessage("message",`Lets get to work ${user.name}!`)
