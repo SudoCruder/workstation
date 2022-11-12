3eb2f643a08f0604267b45acd7bc1a9240ef5400
SudoCruder <117926295+SudoCruder@users.noreply.github.com>
2022-11-12 18:32:56 +0100

    js for listing items

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,22 @@
-let user = {name: "SudoCoder", email: "SudoCoder@email.com"};
+let today = new Date().toDateString();
+let user = {
+  name: 'SudoCoder',
+  email: 'SudoCoder@email.com',
+  displayInfo: function(tragetID) {
+    let userInfo = `${this.name} - ${this.email}`;
+    document.getElementById(tragetID).textContent = userInfo;
+  }
+}
 
-showMessage("message",`Lets get to work ${user.name}!`)
+// Header
+user.displayInfo('user-info')
+showMessage('today-date', today);
+
+// Main
+showMessage('message',`Lets get to work ${user.name}!`)
+
+const goals = ['HTML/CSS', 'Javascript', 'Object-Oriented Programming']
+listItems('goals', goals);
+
+const tasks = ['Tasks #1', 'Tasks #2', 'Tasks #3', 'Tasks #4']
+listItems('tasks', tasks)
--- scripts/utils.js
+++ scripts/utils.js
@@ -3,3 +3,23 @@ function showMessage(tragetID, message) {
     document.getElementById(tragetID).textContent = message;
   }
 }
+
+function listItems(itemsWrapperID, items) {
+  let itemsWrapper = document.getElementById(itemsWrapperID);
+
+  for(let i = 0; i < items.length; i++) {
+    if (itemsWrapper) {
+      let li = document.createElement('li');
+      li.textContent = items[i];
+      itemsWrapper.append(li);
+      li.style.cursor = 'pointer';
+      li.addEventListener('click', function() {
+        if (li.classList.contains('done')) {
+          li.classList.remove('done');
+        } else {
+          li.classList.add('done');
+        }
+      })
+    }
+  }
+}
