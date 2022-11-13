e7282439a27462230508ae38da76e8ce1902cffe
SudoCruder <117926295+SudoCruder@users.noreply.github.com>
2022-11-13 19:46:33 +0100

    add and setup express & ejs

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+// Register ejs as .html. If we did not call this, we would need to name our views foo.ejs instead of foo.html. 
+// The __express method is simply a function that engines use to hook into the Express view system by default, 
+// so if we want to change "foo.ejs" to "foo.html"  we simply pass _any_ function, in this case `ejs.__express`.
+app.engine('.html', require('ejs').__express);
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'html');
+
+// Dummy users
+var users = [
+  { name: 'tobi', email: 'tobi@learnboost.com' },
+  { name: 'loki', email: 'loki@learnboost.com' },
+  { name: 'jane', email: 'jane@learnboost.com' }
+];
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    users: users,
+    title: "Workstation",
+    header: "Some users"
+  })
+})
+
+app.get('/quiz', function(res, res) {
+  res.render('pages/quiz', {
+    title: "Quiz"
+  })
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}...`))
